34cabddc808685a45ce69aadbe9405a7bd29a421
joon jeong <juny.jeong@gmail.com>
2024-04-15 10:39:45 +1200

    completed migration file for sales_products

---
+++ server/db/migrations/20240414223310_sales_products.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('sales_products', (table) => {
+    table.increments('id')
+    table.integer('sales_id')
+    table.integer('product_id')
+    table.integer('sold_qty')
+    table.double('unit_price')
+    table.double('total_price')
+    table.double('discount')
+    table.double('total_payable')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('sales_products')
+}
