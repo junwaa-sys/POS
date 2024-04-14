52afab927f6658ffbbe12affc4e7402f0dabd1df
joon jeong <juny.jeong@gmail.com>
2024-04-15 10:28:31 +1200

    completed migration file for payment_types table

---
+++ server/db/migrations/20240414222537_payment_types.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('payment_types', (table) => {
+    table.increments('id')
+    table.string('payment_type')
+    table.string('currency')
+    table.double('rate')
+    table.timestamps(true, true)
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('payment_types')
+}
