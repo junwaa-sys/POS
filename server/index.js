006b482f036e30856144e082d3807d36d0a32ad4
joon jeong <juny.jeong@gmail.com>
2024-04-14 21:20:45 +1200

    update absolute path for res.sendFile

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const server = express()
 const PORT = process.env.PORT || 3000
 const testRoutes = require('./routes/testRoutes')
@@ -9,7 +10,7 @@ server.use(express.static(__dirname + '/public'))
 server.use('/api/data', testRoutes)
 
 server.get('*', (req, res) => {
-  res.sendFile('./publc/index.html')
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 server.listen(PORT, () => {
