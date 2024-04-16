c8f5c7f1e64c67dcfc9c055adfe15cb717151789
joon jeong <juny.jeong@gmail.com>
2024-04-17 11:25:36 +1200

    user details edit form

--- client/components/users/EditUser.jsx
+++ client/components/users/EditUser.jsx
@@ -1,10 +1,10 @@
-import { Box, Grid, TextField } from '@mui/material'
+import { Box, Container, Grid, TextField } from '@mui/material'
 import React from 'react'
 
 export default function EditUser(userDetails) {
   const detailsToEdit = userDetails.userDetails
   return (
-    <Box>
+    <Container sx={{ height: '100vh', padding: '10px' }}>
       <Grid>
         <TextField
           id="user-id"
@@ -14,7 +14,20 @@ export default function EditUser(userDetails) {
           }}
           defaultValue={detailsToEdit.id}
         />
+        <TextField id="user-first-name" label="First Name" />
+        <TextField id="user-last-name" label="Last Name" />
+        <Box>
+          <Gird>
+            <TextField id="user-password" label="Password" type="password" />
+            <TextField id="new-password" label="New Password" type="password" />
+            <TextField
+              id="confirm-password"
+              label="Confirm Password"
+              type="password"
+            />
+          </Gird>
+        </Box>
       </Grid>
-    </Box>
+    </Container>
   )
 }
